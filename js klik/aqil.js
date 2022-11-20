82b9cf92a555989fa5dcbfb1e32e24f2f9b9154c
aqilrzayevaynisnik <113420690+aqilrzayevaynisnik@users.noreply.github.com>
2022-11-20 12:38:03 +0300

    Add files via upload

---
+++ js klik/aqil.js
@@ -0,0 +1,21 @@
+const aqil1 = document.getElementById('aqil1')
+const aqil2 = document.getElementById('aqil2')
+const aqil3 = document.getElementById('aqil3')
+const aqil4 = document.getElementById('aqil4')
+
+
+aqil1.onclick = () => {
+    document.body.style.backgroundColor='grey'
+}
+
+aqil2.onclick = () => {
+    document.body.style.backgroundColor='red'
+}
+
+aqil3.onclick = () => {
+    document.body.style.backgroundColor='blue'
+}
+
+aqil4.onclick = () => {
+    document.body.style.backgroundColor='yellow'
+}
